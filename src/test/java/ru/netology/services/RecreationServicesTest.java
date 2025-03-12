d1ef3cd05d50ee315c803511c14b380bb15ee547
Pavel Lebedev <p.lebedev8888@gmail.com>
2025-03-12 20:20:03 +0300

    initial

---
+++ src/test/java/ru/netology/services/RecreationServicesTest.java
@@ -0,0 +1,29 @@
+package ru.netology.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+//import ru.netology.services.RecreationServices;
+
+public class RecreationServicesTest {
+
+    @Test
+    public void PoorFreelancer() {
+        RecreationServices service = new RecreationServices();
+        int expected = 3;
+        int result = service.calculate(10_000, 3_000, 20_000);
+
+        Assertions.assertEquals(expected, result);
+
+    }
+
+    @Test
+    public void RichFreelancer() {
+        RecreationServices service = new RecreationServices();
+        int expected = 2;
+        int result = service.calculate(100_000, 60_000, 150_000);
+
+        Assertions.assertEquals(expected, result);
+
+    }
+}
+
